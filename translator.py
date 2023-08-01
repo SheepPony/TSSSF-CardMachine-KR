12e3b42605f3155bd2d2ca65bde977526bf5bba2
SheepPony <sheepponymlp@gmail.com>
2023-08-01 22:54:12 +0900

    Translator for v2 sheets

--- translator.py
+++ translator.py
@@ -9,7 +9,7 @@ def escape(s):
 	return repr(s)[1:-1]
 
 
-with open("TranslationData/mapping.csv","r",encoding="utf-8") as f:
+with open("TranslationData/mapping_v2.csv","r",encoding="utf-8") as f:
 	csv_rows=list(csv.reader(f))
 csv_rows=csv_rows[2:]
 
@@ -18,6 +18,7 @@ for row in csv_rows:
 	k=row[1]
 	v=row[3]
 	keyword_mappings[k]=v
+print(F"Loaded {len(keyword_mappings)} mappings.")
 	
 def apply_mappings(s):
 	for k in keyword_mappings:
@@ -84,62 +85,85 @@ def parse_csv(filepath,is_goal=False):
 				print("DUPLICATE ENTRY!",key)
 			res[key]=dat
 		i+=1
-	print(F"{filepath} parsed: {len(res)} entries")
+	print(F"{filepath} parsed (V1): {len(res)} entries")
+	return res
+	
+def parse_csv_v2(filepath,*,parse_end=None):
+	res=dict()
+	with open(filepath,"r",encoding="utf-8") as f:
+		csv_rows=list(csv.reader(f))
+	
+	if parse_end:
+		csv_rows=csv_rows[:parse_end]
+	csv_rows=csv_rows[1:]
+	for row in csv_rows:
+		key=unwhite(row[1]) # EN Name
+		dat={
+			"name":row[6],
+			"keyword":row[7],
+			"body":row[8],
+			"flavor":row[9]+" - "+row[10]}
+		res[key]=dat
+	print(F"{filepath} parsed (V2): {len(res)} entries")
 	return res
 
-
 translation_data=dict()
-translation_data.update(parse_csv("TranslationData/pony.csv"))
+#translation_data.update(parse_csv("TranslationData/pony.csv"))
+translation_data.update(parse_csv_v2("TranslationData/pony_v2.csv"))
 translation_data.update(parse_csv("TranslationData/goal.csv",is_goal=True))
-translation_data.update(parse_csv("TranslationData/ship.csv"))
+translation_data.update(parse_csv_v2("TranslationData/ship_v2.csv",parse_end=60))
 
 TYPE, PICTURE, SYMBOLS, TITLE, KEYWORDS, BODY, FLAVOR, EXPANSION, CLIENT = range(9)
 
+def translate(ponE,ponK):
+	print(ponE,"to",ponK)
+	with open(ponE,"r",encoding="utf-8") as f:
+		orig_pon=f.read()
 
-with open("TSSSF/Core 1.1.5/cards.pon","r",encoding="utf-8") as f:
-	orig_pon=f.read()
+	orig_lines=orig_pon.split("\n")
+	translated_lines=[]
 
-orig_lines=orig_pon.split("\n")
-translated_lines=[]
-
-print("Translating...")
-for orig_line in orig_lines:
-	orig_tags=orig_line.split('`')
-	#print(orig_tags)
-	
-	if len(orig_tags)<6:
-		translated_lines.append(orig_line)
-		print("Copying line",repr(orig_line))
-		continue
-	title=orig_tags[3]
-	keyword=orig_tags[4]
-	body=orig_tags[5]
-	flavor=orig_tags[6]
-	
-	key=unwhite(unescape(title))
-	#print("Finding",key)
-	if key in translation_data:
+	print("Translating...")
+	for orig_line in orig_lines:
+		orig_tags=orig_line.split('`')
+		#print(orig_tags)
 		
-		dat=translation_data[key]
-		if "name" in dat:
-			title=escape(dat["name"])
-		if "keyword" in dat:
-			keyword=escape(dat["keyword"])
-		if "body" in dat:
-			body=apply_mappings(escape(dat["body"]))
-		if "flavor" in dat:
-			flavor=escape(dat["flavor"])
+		if len(orig_tags)<6:
+			translated_lines.append(orig_line)
+			print("Copying line",repr(orig_line))
+			continue
+		title=orig_tags[3]
+		keyword=orig_tags[4]
+		body=orig_tags[5]
+		flavor=orig_tags[6]
 		
-		#print("  Trans data found:",dat)
-	else:
-		print(" ### No trans data found!",key)
-	
-	translated_tags=orig_tags[:3]+[title,keyword,body,flavor]+orig_tags[7:]
-	#print(translated_tags)
-	
-	translated_lines.append('`'.join(translated_tags))
-print(F"Translated {len(translated_lines)} entries.")
+		key=unwhite(unescape(title))
+		#print("Finding",key)
+		if key in translation_data:
+			
+			dat=translation_data[key]
+			if "name" in dat:
+				title=escape(dat["name"])
+			if "keyword" in dat:
+				keyword=escape(dat["keyword"])
+			if "body" in dat:
+				body=apply_mappings(escape(dat["body"]))
+			if "flavor" in dat:
+				flavor=escape(dat["flavor"])
+			
+			#print("  Trans data found:",dat)
+		else:
+			print(" ### No trans data found!",key)
+		
+		translated_tags=orig_tags[:3]+[title,keyword,body,flavor]+orig_tags[7:]
+		#print(translated_tags)
+		
+		translated_lines.append('`'.join(translated_tags))
+	print(F"Translated {len(translated_lines)} entries.")
 
 
-with open("TSSSF/Core 1.1.5/cardsKR.pon","w",encoding="utf-8") as f:
-	f.write("\n".join(translated_lines))
+	with open(ponK,"w",encoding="utf-8") as f:
+		f.write("\n".join(translated_lines))
+
+translate("TSSSF/Core 1.1.5/cards.pon","TSSSF/Core 1.1.5/cardsKR.pon")
+translate("TSSSF/Extra Credit 1.0.1/cards.pon","TSSSF/Extra Credit 1.0.1/cardsKR.pon")
